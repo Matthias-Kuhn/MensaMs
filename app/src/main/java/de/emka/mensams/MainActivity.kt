f4bc6bd8ee291838696e106a24ac6ed95ae54b9b
Matthias Kuhn <kuhn.matthias@protonmail.com>
2022-11-13 23:26:47 +0100

    added fragments for bnv

--- app/src/main/java/de/emka/mensams/MainActivity.kt
+++ app/src/main/java/de/emka/mensams/MainActivity.kt
@@ -2,10 +2,40 @@ package de.emka.mensams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import de.emka.mensams.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.selectedItemId = R.id.home
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            Log.d("MainActivity", "navbar selection changed")
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.about -> replaceFragment(AboutFragment())
+                R.id.options -> replaceFragment(OptionsFragment())
+
+
+                else -> {}
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
